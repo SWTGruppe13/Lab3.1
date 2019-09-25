76cc2041476789af0884521ef4db6c8018ffaf4c
Emil Knudsen <eknudsen1992@hotmail.com>
2019-09-25 13:22:38 +0200

    Substitute version added

---
+++ ECS.Test.Unit.Subtitute/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ECS.Test.Unit.Subtitute")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ECS.Test.Unit.Subtitute")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a78cdc5f-bb5c-4930-ad12-c6e8dda3dbcc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ECS.Unit.Test.Substitute/ECSTestUnitSubstitute.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECS.Legacy;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace ECS.Test.Unit.Substitute
+{
+    public class ECSTestUnitSubstitute
+    {
+
+        public IHeater heater;
+        public ITempSensor temp;
+        private ECS.Legacy.ECS uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            heater = NSubstitute.Substitute.For<IHeater>();
+            temp = NSubstitute.Substitute.For<ITempSensor>();
+            uut = new ECS.Legacy.ECS(45, heater, temp);
+        }
+
+        [Test]
+        public void RunSelfTest_ReturnsFalse()
+        {
+            temp.RunSelfTest().Returns(false);
+            heater.RunSelfTest().Returns(true);
+            Assert.IsFalse(uut.RunSelfTest());
+        }
+
+        [Test]
+        public void Temp_Equal_To_30()
+        {
+            temp.GetTemp().Returns(30);
+            Assert.That(uut.GetCurTemp().Equals(30));
+        }
+
+        [Test]
+        public void Regulate_Below_Threshold_TurnOn_Called_Once()
+        {
+            temp.GetTemp().Returns(15);
+            uut.Regulate();
+            heater.Received(1).TurnOn();
+        }
+
+        [Test]
+        public void Regulate_Above_Threshold_TurnOff_Called_Once()
+        {
+            temp.GetTemp().Returns(45);
+            uut.Regulate();
+            heater.Received(1).TurnOff();
+        }
+    }
+}
